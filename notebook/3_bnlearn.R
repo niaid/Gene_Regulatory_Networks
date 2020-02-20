106ebeca7b286217c2f273a6b0c87d7d5ba964bf
zhuy16 <zhuy16@nih.gov>
2020-02-20 18:33:18 -0500

    updated night before

--- notebook/3_bnlearn.R
+++ notebook/3_bnlearn.R
@@ -19,14 +19,14 @@ graphviz.plot(dag,shape = "ellipse")
 dag$nodes
 
 plot(dag,shape = "ellipse")
-?plot()
+
 
 ##########################################################
 ### #write.csv(exprMat_filtered_log,"/Users/zhuy16/GRN/Gene_Regulatory_Networks/data/exprMat_filtered_log.csv")
 # 
 
 
-exprMatRaw=read.csv("/Users/zhuy16/GRN/Gene_Regulatory_Networks/data/exprMat_filtered_log.csv",row.names = 1,header = T)
+exprMatRaw=read.csv("../data/exprMat_filtered_log.csv",row.names = 1,header = T)
 nrow(exprMatRaw)
 ncol(exprMatRaw)
 exprMat1=exprMatRaw[rowMeans(exprMatRaw)>1.8,]
@@ -35,7 +35,7 @@ ncol(exprMat1)
 exprMat1[1:5,1:5]
 #BiocManager::install("Rgraphviz")
 library(Rgraphviz)
-str(exprMat_filtered_log)str
+str(exprMat1)
 dag = hc(data.frame(t(exprMat1)))
 #dag = hc(data.frame(t(exprMat_filtered_log)))
 plot(dag)
@@ -47,7 +47,7 @@ graphviz.plot(dag,layout="dot",shape = "ellipse")
 length(dag$arcs)
 
 getwd()
-write.csv(dag$arcs,"/Users/zhuy16/GRN/Gene_Regulatory_Networks/data/arcsInTheBayesNet.csv")
+write.csv(dag$arcs,"../data/3_BNLEARN/arcsInTheBayesNet.csv")
 
 dag$nodes[1:5]
 ?graphviz.plot()
