ffbdd222b5af92395a80afaf635374eb5d9d99ed
zhuy16 <zhuy16@nih.gov>
2020-02-27 05:09:10 -0500

    session_2

--- session_2/RunSCENICsimple.R
+++ session_2/RunSCENICsimple.R
@@ -13,7 +13,7 @@ exprMat <- get_dgem(loom)
 
 class(exprMat)
 exprMat[1:5,1:5]
-hist(rowMeans(exprMat))
+hist(log10(rowMeans(exprMat)+1))
 
 cellInfo <- get_cellAnnotation(loom)
 close_loom(loom)
@@ -21,6 +21,10 @@ close_loom(loom)
 #############################################################
 ### scenicOptions works like a manager throughout the run to store results and give direction
 ### as to which location to save the results. 
+################
+
+################
+## the data base folder is on the server /home/bcbb_teaching_files/zhuy/cisTarget_databases  
 ################
 scenicOptions <- initializeScenic(org="mgi", dbDir="./cisTarget_databases", nCores=10)
 #### scenicOption is a S4 object to store all the variable and folder names, 
@@ -53,7 +57,7 @@ x[1:5,1:5]
 exprMat_filtered_log <- log2(exprMat_filtered+1) 
 ## take 1 minutes to run
 runGenie3(exprMat_filtered_log, scenicOptions)
-### to check what is the problem with the dataset. 
+
 # ?runGenie3()
 # genie3wm                          "int/1.3_GENIE3_weightMatrix.Rds"              
 # genie3ll                          "int/1.4_GENIE3_linkList.Rds"   
@@ -61,26 +65,13 @@ runGenie3(exprMat_filtered_log, scenicOptions)
 ################## this is important to show the integrated interaction ranking of all the TF-targets. 
 x=readRDS("int/1.4_GENIE3_linkList.Rds")
 str(x)
-fix(x)
-x=readRDS("int/1.3_GENIE3_weightMatrix_part_8.Rds")
-
-# allTFs <- getDbTfs(scenicOptions)
-# inputTFs <- allTFs[allTFs %in% rownames(exprMat)] 
-# percMatched <- length(inputTFs)/length(allTFs)
-
-
+head(x)
 
 runSCENIC_1_coexNetwork2modules(scenicOptions)
 ?runSCENIC_1_coexNetwork2modules()
 
-x=readRDS("int/1.6_tfModules_asDF.Rds")
-
-### this become coexpression modules with TF in front. 
-class(x)
-length(x)
-x[[4]]
 ##################################################################
-## this step takes 2 minutes
+## this step takes 2 minutes, it runs cisTarget to find out the top confident upstream TFBSs. 
 scenicOptions@settings$dbs <- scenicOptions@settings$dbs["10kb"] # Toy run settings
 runSCENIC_2_createRegulons(scenicOptions, coexMethod=c("top5perTarget")) # Toy run settings
 ?runSCENIC_2_createRegulons()
